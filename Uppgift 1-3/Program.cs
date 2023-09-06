31a9a07a32b324bca262893c0608e2a37d4ca795
thbr06001 <thbr06001@E-PW0234MD>
2023-09-06 11:04:23 +0200

    Add project files.

---
+++ Uppgift 1-3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift1_._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\"); //Har gjort en triangel
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+            Console.ReadKey();
+        }
+    }
+}
